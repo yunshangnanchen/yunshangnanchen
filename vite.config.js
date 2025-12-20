1c1a44f1d5f1ae4ca762cf277a67ba6e05dbf99a
Nanchen jiajia <130617328+yunshangnanchen@users.noreply.github.com>
2025-12-20 18:10:02 +0800

    updata

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
-  base: '/',
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
+  base: '/yunshangnanchen/',
 })
